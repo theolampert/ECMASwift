ffb664143097c7e5b0b27390c55e93c6549f879c
Theo <mail@theo.sh>
2023-05-13 08:45:00 +0200

    Setup project

---
+++ Sources/ECMASwift/ECMASwift.swift
@@ -0,0 +1,6 @@
+public struct ECMASwift {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ Tests/ECMASwiftTests/ECMASwiftTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import ECMASwift
+
+final class ECMASwiftTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(ECMASwift().text, "Hello, World!")
+    }
+}
