9ea8b62d0d2a2e244bf74428072c1aa5bebf24c2
Theo <mail@theo.sh>
2023-06-26 20:57:30 +0200

    Rough url api

---
+++ Sources/ECMASwift/API/URL/URL.swift
@@ -0,0 +1,79 @@
+//
+//  URL.swift
+//  
+//
+//  Created by Theodore Lampert on 26.06.23.
+//
+
+import Foundation
+import JavaScriptCore
+
+@objc protocol URLExports: JSExport {
+    var `protocol`: String { @objc get @objc set }
+    var hostname: String { @objc get @objc set }
+    var pathname: String { @objc get @objc set }
+    var searchParams: URLSearchParams { @objc get @objc set }
+    func toString() -> String
+}
+
+class URL: NSObject, URLExports {
+    var url: Foundation.URL?
+    
+    init(string: String) {
+        super.init()
+        url = Foundation.URL(string: string)
+    }
+    
+    init(url: Foundation.URL) {
+        super.init()
+        self.url = url
+    }
+    
+    func toString() -> String {
+        if let url = url, !searchParams.toString().isEmpty {
+            return url.absoluteString + "?" + searchParams.toString()
+        }
+        return url?.absoluteString ?? ""
+    }
+    
+    var `protocol`: String {
+        get {
+            return url?.scheme ?? ""
+        }
+        set(newValue) {
+            // TODO
+        }
+    }
+    
+    var hostname: String {
+        get {
+            return url?.host ?? ""
+        }
+        set(newValue) {
+            // TODO
+        }
+    }
+    
+    var pathname: String {
+        get {
+            return url?.path ?? ""
+        }
+        set(newValue) {
+            // TODO
+        }
+    }
+    
+    var searchParams: URLSearchParams = URLSearchParams()
+}
+
+public struct URLAPI {
+    public func registerAPIInto(context: JSContext) {
+        let urlClass: @convention(block) (String) -> URL = { string in
+            URL(string: string)
+        }
+        context.setObject(
+            unsafeBitCast(urlClass, to: AnyObject.self),
+            forKeyedSubscript: "URL" as NSString
+        )
+    }
+}
--- Sources/ECMASwift/Helpers/JSContext+callAsyncFunction.swift
+++ Sources/ECMASwift/Helpers/JSContext+callAsyncFunction.swift
@@ -33,4 +33,30 @@ public extension JSContext {
             promise?.invokeMethod("then", withArguments: promiseArgs)
         }
     }
+    
+    func invokeAsyncMethod(key: String, methodKey: String, withArguments: [Any] = []) async throws -> JSValue {
+        try await withCheckedThrowingContinuation { continuation in
+            let onFulfilled: @convention(block) (JSValue) -> Void = {
+                continuation.resume(returning: $0)
+            }
+            let onRejected: @convention(block) (JSValue) -> Void = {
+                let error = NSError(
+                    domain: key,
+                    code: 0,
+                    userInfo: [NSLocalizedDescriptionKey: "\($0)"]
+                )
+                continuation.resume(throwing: error)
+            }
+            let promiseArgs = [
+                unsafeBitCast(onFulfilled,
+                              to: JSValue.self),
+                unsafeBitCast(onRejected,
+                              to: JSValue.self),
+            ]
+
+            let promise = self.objectForKeyedSubscript(key)
+                .invokeMethod(methodKey, withArguments: withArguments)
+            promise?.invokeMethod("then", withArguments: promiseArgs)
+        }
+    }
 }
